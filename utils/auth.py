1e4a82c66947bc3793b318afbd515b8573b75eff
Jacopo Ghisleni <jacopo.ghisleni@gmail.com>
2025-04-21 15:15:28 +0200

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,14 +1,12 @@
 import streamlit as st
 
 def check_login(username, password):
-    # Dizionario utenti: username → (password, ruolo)
     users = {
         "admin": ("password1", "bionic"),
         "cittadino": ("password2", "responsabile")
     }
 
     if username in users and users[username][0] == password:
-        return True, users[username][1]  # login OK, ritorna anche il ruolo
+        return True, users[username][1]
     else:
         return False, None
-
