a742f0b768413a17ef3f2592a71c9824b203c029
Jacopo Ghisleni <jacopo.ghisleni@gmail.com>
2025-04-24 08:38:34 +0200

    Update google_sheet.py

--- lib/google_sheet.py
+++ lib/google_sheet.py
@@ -1,5 +1,6 @@
-import gspread
+import os
 import json
+import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
 
@@ -8,11 +9,19 @@ def get_gspread_client():
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    service_account_info = (st.secrets["gcp_service_account"])
+
+    # Lettura delle credenziali: Streamlit Cloud o Cloud Run
+    if "gcp_service_account" in st.secrets:
+        service_account_info = st.secrets["gcp_service_account"]
+    elif "GCP_CREDENTIALS_JSON" in os.environ:
+        service_account_info = json.loads(os.environ["GCP_CREDENTIALS_JSON"])
+    else:
+        st.error("❌ Credenziali Google non trovate.")
+        st.stop()
+
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(service_account_info, scope)
     return gspread.authorize(credentials)
 
-# lib/google_sheet.py
 
 def get_sheet_by_name(sheet_name, worksheet_name="Sheet1"):
     client = get_gspread_client()
@@ -22,4 +31,3 @@ def get_sheet_by_name(sheet_name, worksheet_name="Sheet1"):
     except Exception as e:
         st.error(f"❌ Errore nel caricamento del foglio '{worksheet_name}': {e}")
         return None
-
